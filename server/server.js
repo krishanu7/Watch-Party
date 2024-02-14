f18dabe184cae951990b8091d5c57f6147eb1b19
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-02-14 18:37:58 +0530

    Updated Server

--- server/server.js
+++ server/server.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const ioUtils = require('./utils/io')
+const ioUtils = require('./utils/io');
 
-const io = require('socket.io')(server,{
-    path: '/socket',
-    origin: ['http://localhost:3000'],
-    serverClient: false,
-})
-
-const cors = require('cors');
-app.use(cors({origin: 'http://localhost:3000'}));
-
-const PORT = process.env.PORT ||8080;
-
-app.get('/test', (req, res, next) => {
-    res.send({message: "Hello World"});
+const io = require('socket.io')(server, {
+  path: '/socket',
+  origins: ['http://localhost:3000'], 
+  serveClient: false,
 });
 
-ioUtils.setUpIO(io);
+const cors = require('cors');
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res, next) => {
+  res.send({ message: 'Hello World' });
+});
+
+ioUtils.setupIO(io);
+
+server.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
--- server/utils/Room.js
+++ server/utils/Room.js
@@ -1,63 +1,78 @@
-let rooms = {}; // { users: [...{name, id, roomId}], videoURL: ''}
-let userMap = {}; // maps socket id to rooms
-
-const addRoom = (roomId, videoId) => {
-  if (!rooms[roomId]) rooms[roomId] = { users: [], videoId };
-};
-const setVideoId = (roomId, videoId) => {
-  if (rooms[roomId]) rooms[roomId]["videoId"] = videoId;
-};
-const getRoom = (roomId) => rooms[roomId];
-
-const addUser = (roomId, name, userId) => {
-  rooms[roomId]["userId"].push({ name, id: userId, roomId });
-  userMap[userId] = roomId;
-};
-const removeUser = (userId) => {
-  const roomId = userMap[userId];
-  let _user = null;
-  if (roomId) {
-    const users = rooms[roomId]["users"];
-    rooms[roomId]["users"] = users.filter((user) => {
-      if (user.id === userId) {
-        _user = user;
-      }
-      return user.id !== userId;
-    });
-    delete userMap[userId];
-    removeRoom(roomId);
-    return _user;
+class Rooms {
+  constructor() {
+    this.rooms = {}; //{ users: [...{name, id, roomId}], videoURL: ''}
+    this.userMap = {}; // socket.id -> room.id (map)
   }
-  return null;
-};
 
-const removeRoom = (roomId) => {
-  if (rooms[roomId]["users"].length === 0) delete rooms[roomId];
-};
-const getUserList = (roomId) => {
-  const room = rooms[roomId];
-  return room ? room["users"] : [];
-};
-const getUser = (userId) => {
-  const roomId = userMap[userId];
-  const users = getUserList(roomId);
-  return users.find((user) => user.id === userId);
-};
-const showInfo = () => {
-  const roomIds = Object.keys(rooms);
-  roomIds.forEach((roomId) => {
-    console.log(`Room: ${roomId}`);
-    rooms[roomId]["users"].forEach((user) => console.log(user));
-  });
-};
-module.exports = {
-  addRoom,
-  setVideoId,
-  getRoom,
-  addUser,
-  removeUser,
-  removeRoom,
-  getUserList,
-  getUser,
-  showInfo,
-};
+  addRoom(roomId, videoId) {
+    if (!this.rooms[roomId]) this.rooms[roomId] = { users: [], videoId };
+  }
+
+  setVideoId(roomId, videoId) {
+    if (this.rooms[roomId]) {
+      this.rooms[roomId]["videoId"] = videoId;
+    }
+  }
+
+  getRoom(roomId) {
+    return this.rooms[roomId];
+  }
+
+  addUser(roomId, name, userId) {
+    this.rooms[roomId]["users"].push({ name, id: userId, roomId });
+    this.userMap[userId] = roomId;
+  }
+
+  removeUser(userId) {
+    const roomId = this.userMap[userId];
+    let _user = null;
+
+    if (roomId) {
+      // remove user from the room
+      const users = this.rooms[roomId]["users"];
+      this.rooms[roomId]["users"] = users.filter((user) => {
+        if (user.id === userId) {
+          _user = user;
+        }
+        return user.id !== userId;
+      });
+
+      // remove user from the user-room mapping
+      delete this.userMap[userId];
+
+      // remove room if applicable
+      this.removeRoom(roomId);
+
+      return _user;
+    }
+
+    return null;
+  }
+
+  removeRoom(roomId) {
+    if (this.rooms[roomId]["users"].length === 0) delete this.rooms[roomId];
+  }
+
+  getUserList(roomId) {
+    const room = this.rooms[roomId];
+    if (room) {
+      return room["users"];
+    }
+  }
+
+  getUser(userId) {
+    const room = this.userMap[userId];
+    const users = this.getUserList(room);
+    return users.find((user) => user.id === userId);
+  }
+
+  showInfo() {
+    const rooms = Object.keys(this.rooms);
+    rooms.forEach((roomId) => {
+      console.log(`Room: ${roomId}`);
+      this.rooms[roomId]["users"].forEach((user) => console.log(user));
+    });
+  }
+}
+
+module.exports = new Rooms();

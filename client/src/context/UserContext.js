d545ca5ca8e332276eb01e1765f692e568f5b26f
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-02-18 20:52:02 +0530

    NewField added in Context

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,21 +1,20 @@
-import React, {createContext, useReducer, useState} from 'react';
-import { userReducer } from '../reducers/userReducer';
+import React, { createContext, useReducer, useState } from "react";
+import { userReducer } from "../reducers/userReducer";
 
 export const UserContext = createContext();
 
 export const UserContextProvider = (props) => {
-    const initialState = {
-        userList : [],
-        message : [],
-        videoId : '',
-        username : '',
-        socket: null,
-    };
-    const [userData, dispatch] = useReducer(userReducer, initialState);
-    const [socket, setSocket] = useState(null);
-    return (
-        <UserContext.Provider value={{ userData, dispatch , socket, setSocket}}>
-            {props.children}
-        </UserContext.Provider>
-    );
-}
+  const initialState = {
+    userList: [],
+    message: [],
+    videoId: "",
+    username: "",
+    socket: null,
+  };
+  const [userData, dispatch] = useReducer(userReducer, initialState);
+  return (
+    <UserContext.Provider value={{ userData, dispatch }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
